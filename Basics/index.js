8b507bc77ffb2e7be864025469ed6a642f619e09
AJAY YADAV <imajydv8298@gmail.com>
2023-04-08 15:22:08 +0530

    Creating react app using CDN links

---
+++ Basics/index.js
@@ -0,0 +1,38 @@
+// Step 1: Create an HTML element using ReactJS.
+
+const element = React.createElement("div", { class: "parent" }, [
+  React.createElement("div", { Style: "background:red", class: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "heading1" },
+      "This is my heading-1 from child-1"
+    ),
+    React.createElement(
+      "h1",
+      { id: "heading2" },
+      "This is my heading-2 from child-1"
+    ),
+  ]),
+  React.createElement(
+    "div",
+    { Style: "background:green; color:white", class: "child2" },
+    [
+      React.createElement(
+        "h1",
+        { id: "heading1" },
+        "This is my heading-1 from child-2"
+      ),
+      React.createElement(
+        "h1",
+        { id: "heading2" },
+        "This is my heading-2 from child-2"
+      ),
+    ]
+  ),
+]);
+
+// Step 2: Create a Root element using ReactJS.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Step 3: Render all HTML elements using ReactJS on the web page.
+root.render(element);
